936159270badbb05f12b3415506eaddd0d9fb424
ChangMin <cckdals222@gmail.com>
2023-04-27 13:10:11 +0900

    feat: tag badge 추가

--- src/common/components/detail/Info.tsx
+++ src/common/components/detail/Info.tsx
@@ -37,7 +37,7 @@ export const Info = ({ info }: InfoProps) => {
               />
             ))}
           </div>
-          <span>({info.averageRating})</span>
+          <span>({info.averageRating}점)</span>
           <span>{info.comments.length}개의 수강평</span>
         </div>
         <div className="flex items-center space-x-1">
@@ -48,6 +48,16 @@ export const Info = ({ info }: InfoProps) => {
         <span className="text-lg">
           강의시간: {Math.floor(info.duration / 60)}시간 {info.duration % 60}분
         </span>
+        <div className="flex space-x-1 pt-1">
+          {info.tags.map(({ name }, index) => (
+            <span
+              key={index}
+              className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300"
+            >
+              {name}
+            </span>
+          ))}
+        </div>
       </div>
       <div className="flex w-full h-36 bg-green-500 text-white text-3xl justify-center items-center">
         {info.subCategory.name}
